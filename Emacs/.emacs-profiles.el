db1924767064f9f27372586d70bd5a5dec4f0944
UncleAlone <desmond.pc.w@gmail.com>
2022-02-21 23:05:32 -0500

    add Eamcs configuration

---
+++ Emacs/.emacs-profiles.el
@@ -0,0 +1,6 @@
+(("default" . ((user-emacs-directory . "~/.dotfiles/Emacs/emacs-configs/demacs")))
+ ("doom" . ((user-emacs-directory . "~/.dotfiles/Emacs/emacs-configs/doom-core")
+	    ;;(env . (("DOOMDIR" . "~/.dotfiles/Emacs/emacs-configs/doom")))))
+	    ))
+ ("beta" . ((user-emacs-directory . "~/.dotfiles/Emacs/emacs-configs/beta_emacs")))
+ )
